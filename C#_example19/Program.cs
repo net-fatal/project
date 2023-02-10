72dd8e79af8355b1e803c6a89688fc1b2ba1ede5
Ulugbek <ulugbek.kasymov@gmail.com>
2023-02-10 18:28:01 +0300

    add убрал ошибки

--- C#_example19/Program.cs
+++ C#_example19/Program.cs
@@ -9,16 +9,15 @@ Console.Clear();
 
 
 Console.Write("Введите число: ");
-    // int num = int.Parse(Console.ReadLine()!); 
-    // int t, r;
-    // int sum = 0;
-    // for(t = num;num != 0;num = num/10){
-    //      r = num % 10;
-    //      sum = sum*10+r;
-    // }
-    // if(t==sum)
-    //      Console.Write("Число {0} является полиндромом);
-    // else
-    //      Console.Write("Число {0} не является полиндромом);
+int num = int.Parse(Console.ReadLine()!); 
+int t, r;
+int sum = 0;
+    for(t = num;num != 0;num = num/10){
+        r = num % 10;
+        sum = sum*10+r;
+    }
+    if(t==sum)
+Console.Write("Число {0} является полиндромом");
+    else
+Console.Write("Число {0} не является полиндромом");
 
-    int x = int.Parse(Console.ReadLine()!);
