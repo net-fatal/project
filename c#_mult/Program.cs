530bfec1012324b11b822d706facc64b289cd566
Ulugbek <ulugbek.kasymov@gmail.com>
2023-02-11 17:10:53 +0300

    add c#_mult
    Console.Clear();
    
    Console.Write("Введите число N: ");
    int num = int.Parse(Console.ReadLine()!);
    int result = 1;
    for (int i = 1; i <= num; i++)
    {
         result = result * i;
    }
    Console.WriteLine($"Произведение чмсел от 1 до N равно {result}")

---
+++ c#_mult/Program.cs
@@ -0,0 +1,12 @@
+﻿// Напишите программу вычисления произведения чисел от 1 до N
+
+Console.Clear();
+
+Console.Write("Введите число N: ");
+int num = int.Parse(Console.ReadLine()!);
+int result = 1;
+for (int i = 1; i <= num; i++)
+{
+     result = result * i;
+}
+Console.WriteLine($"Произведение чмсел от 1 до N равно {result}");
