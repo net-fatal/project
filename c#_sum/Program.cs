997d282a5999932093c0b27109dcfd521657c422
Ulugbek <ulugbek.kasymov@gmail.com>
2023-02-11 18:07:51 +0300

    add c#_sum

---
+++ c#_sum/Program.cs
@@ -0,0 +1,14 @@
+﻿//Посчитайте сумму цифр в числе
+
+Console.Clear();
+
+Console.Write("Введите число: ");
+int num = int.Parse(Console.ReadLine()!);
+int sum = 0;
+while (num > 0)
+{
+     sum = sum + num%10;
+     num = (num/10);
+}
+Console.WriteLine($"Сумма числа равна {sum}");
+
