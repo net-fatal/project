ea18b080e0bc9624fdfec949818127b20bb5c820
Ulugbek <ulugbek.kasymov@gmail.com>
2023-02-17 19:00:55 +0300

    add c#_example34

---
+++ c#_example34/Program.cs
@@ -0,0 +1,27 @@
+﻿// Задайте массив заполненный случайными положительными трёхзначными числами. 
+//Напишите программу, которая покажет количество чётных чисел в массиве.
+
+
+int[] arr = new int[12];
+int k = 0, sum = 0;
+Random rnd = new Random();
+       
+Console.WriteLine("Исходный массив");
+for (int i = 0; i < 12; i++)
+{
+    arr[i] = rnd.Next(10, 50);
+    Console.Write("{0} ", arr[i]);
+}
+
+       
+foreach(int element in arr)
+{
+Console.Write("{0} ", element);
+if (element % 2 == 0)
+{
+    k++;
+    sum += element;
+}
+}
+Console.WriteLine();
+Console.WriteLine($"Количество четных элементов {k}, сумма четных элементовn {sum}");
