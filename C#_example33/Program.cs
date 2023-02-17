317d027e640450f2b68f3bc5032e9269f755793c
Ulugbek <ulugbek.kasymov@gmail.com>
2023-02-17 19:25:57 +0300

    add c#_example36

---
+++ C#_example33/Program.cs
@@ -0,0 +1,33 @@
+﻿// /Задайте массив Напишите программу которая определяет присутсвует ли 
+// заданное число в массиве
+
+
+int[] GetArray(int size, int minValue, int maxValue)
+{
+    int[] result = new int[size];
+
+    for (int i = 0; i < size; i++)
+    {
+        result[i] = new Random().Next(minValue, maxValue+1);
+    }
+    return result;
+}
+
+bool FindNumber(int[] inArray, int number)
+{
+    foreach(int element in inArray)
+    {
+        if(element == number)
+        return true;
+    }
+    return false;
+}
+
+int[] arr = GetArray(5, - 10, 10);
+Console.WriteLine(String.Join(", ", arr));
+int chiclo = int.Parse(Console.ReadLine());
+if (FindNumber(arr, chiclo))
+{
+    Console.WriteLine("Да");
+}
+else Console.WriteLine("Нет");
